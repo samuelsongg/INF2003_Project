1355c62a440b58d9da9eec522ec4e0c888db2144
Samuel Song <45842379+samuelsongg@users.noreply.github.com>
2023-10-03 20:34:08 +0800

    Added admin account

--- init_db.py
+++ init_db.py
@@ -2,14 +2,22 @@
 # Do NOT run it unless you want to reset the database.
 
 import sqlite3
+from werkzeug.security import generate_password_hash
 
-connection = sqlite3.connect('sql_database.db')
+conn = sqlite3.connect('sql_database.db')
 
 with open('schema.sql') as f:
-    connection.executescript(f.read())
+    conn.executescript(f.read())
 
-# connection.commit()
-connection.close()
+# Create an admin account.
+password = 'admin'
+password = generate_password_hash(password, method='pbkdf2:sha256')
+conn.execute('INSERT INTO users (first_name, email, password, account_type) VALUES (?, ?, ?, ?)', 
+            ('administrator', 'admin@gmail.com', password, 'admin'))
+            
+
+conn.commit()
+conn.close()
 
 # Troubleshooting purposes.
 print('Database initialized.')
